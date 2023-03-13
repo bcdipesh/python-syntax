76828d6bcdccf93aa455b01db7254291d427c32e
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-13 08:32:46 -0400

    Implement sum_nums function

--- sum.py
+++ sum.py
@@ -6,12 +6,17 @@ def sum_nums(nums):
 
     Should return (not print):
       10
-    """  
+    """
 
-    # Python has a built-in function `sum()` for this, but we don't
-    # want you to use it. Please write this by hand.
+    # create and initialize a total_sum to 0
+    total_sum = 0
+    # loop over each num in nums list
+    for num in nums:
+        # add the current num to total_sum and store the result to total_sum
+        total_sum += num
 
-    # YOUR CODE HERE
+    # return total_sum
+    return total_sum
 
 
 print("sum_nums returned", sum_nums([1, 2, 3, 4]))
